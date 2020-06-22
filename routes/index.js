38548cb6fbd880469203dfc83dfd7d02ed5f1003
Milan <kamboyamilan@gmail.com>
2020-06-22 19:42:46 +0530

    initial routes setup

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const homeRouter = require('./home');
+
+const router = express.Router();
+
+router.use('/', homeRouter);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/dbSetup');
+const router = require('./routes/index');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -8,6 +9,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
