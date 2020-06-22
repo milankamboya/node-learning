127e92c55133786dd8f629762092cd72fdf7fac8
Milan <kamboyamilan@gmail.com>
2020-06-22 18:52:40 +0530

    add mongo db connection setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/dbSetup');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
