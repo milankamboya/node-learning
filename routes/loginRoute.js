9769d263f69521aa92f0cc9a819346cf97982d7d
Milan <kamboyamilan@gmail.com>
2020-06-27 22:49:31 +0530

    added api to register user

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -3,6 +3,17 @@ const UserMaster = require('../models/user');
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
 
+  const tempUser = await UserMaster.findOne({ email });
+  if (tempUser) {
+    return res.json('user already exists');
+  }
+
+  const User = new UserMaster({ name, email, password });
+  let newUser = await User.save();
+  return res.json({ _id: newUser._id, name: newUser.name, email: newUser.email, date: newUser.date, role: newUser.role });
 });
+
+module.exports = router;
