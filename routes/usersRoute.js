96c54618d926403be26a0aa1b75d03e14787a8d6
Milan <kamboyamilan@gmail.com>
2020-06-27 22:50:33 +0530

    added missing return statement in api

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const users = [{ name: 'Guru', age: '27' }, { name: 'Backy', age: '27' }];
-  res.json(users);
+  return res.json(users);
 });
 
 module.exports = router;
